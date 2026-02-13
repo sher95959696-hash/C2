9f77b4727d87816752732ca5d7cf528a60ecb765
sher95959696-hash <sher95959696@gmail.com>
2026-02-13 07:48:45 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   // UNIQUE App ID for the client
-  appId: 'com.Razor's Edge Ltd', 
+  appId: 'com.RazorsEdge.Ltd', 
   
   // Name displayed under the icon
   appName: 'Razor's Edge Ltd', 
